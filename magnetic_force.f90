ab0e0b53baf73cc869050b22cc2f0a5a4f37631d
María Fiora <fiora@sol19.tandar.cnea.gov.ar>
2017-11-23 14:46:22 -0300

    * magnetic_force modified to test force implemmentation WARNING: MD destroyed !!!

--- magnetic_force.f90
+++ magnetic_force.f90
@@ -12,10 +12,10 @@ subroutine magnetic_force(mode)
         select case(mode)
         case(0)   ! Initialization 
 
-            m_z = 1.0  ! Magnetic moment of the particle
+            m_z = 10.0  ! Magnetic moment of the particle
             V_part = 4.*pi*(sigma(4,4)/2.)**3/3. ! Volume of the particle
             r_loop_min =1.0 ! Minimum radius of the loop
-            r_loop_max=1.0  ! maximun radius of the loop
+            r_loop_max=2.0  ! maximun radius of the loop
             i_loop=1.0 ! Current of the loop
             n_loop=1   ! Number of turns of the loop
             cte=n_loop*mu_0/(2*pi*2*(r_loop_max-r_loop_min))
@@ -24,16 +24,21 @@ subroutine magnetic_force(mode)
             print *,"Magnetic momemtum of particles =",m_z
 
         case(1)  ! Compute magnetic force on particles 
-            do i_part = part_init_e + 1, n_part
-
+!            do i_part = part_init_e + 1, n_part
+                i_part= part_init_e + 1
+                r0(3,i_part) = 3.0  ! debug
                 mag_force=cte*(log(sqrt((r_loop_max**2+r0(3,i_part)**2))+ & 
                     r_loop_max/sqrt((r_loop_min**2+r0(3,i_part)**2))+r_loop_min)- & 
                     r_loop_max/sqrt(r_loop_max**2+r0(3,i_part)**2)-      & 
                     r_loop_min/sqrt(r_loop_min**2+r0(3,i_part)**2))
+               
+                  print *,'mag_force=',mag_force ! debug
+
+                  stop !debug
 
                 force(3,i_part) = force(3,i_part) + mag_force
 
-            end do
+!            end do
         end select
                 
 
